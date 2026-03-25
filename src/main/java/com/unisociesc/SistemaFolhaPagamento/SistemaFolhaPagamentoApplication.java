e44f6267e554f3d64874b9273b9287f7c038e43a
VINICIUS DOS SANTOS ZAPELLA <vinicius_zapella@estudante.sesisenai.org.br>
2026-03-25 14:24:56 -0300

    first commit

---
+++ src/main/java/com/unisociesc/SistemaFolhaPagamento/SistemaFolhaPagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.unisociesc.SistemaFolhaPagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFolhaPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFolhaPagamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unisociesc/SistemaFolhaPagamento/SistemaFolhaPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisociesc.SistemaFolhaPagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaFolhaPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
